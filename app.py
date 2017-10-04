a331b4688d1d9a44ee4937a17afa35b1230debb1
Bruno Bonando <bruno@bonandos.com>
2017-10-04 01:35:38 -0300

    Refactoring Gen Key Fnc

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from subprocess import call
 
 call(['clear'])
 call(['echo', 'Bonandos:/# This system is about to collect data from all machines in our intranet environment.'])
-utils.generate_keys('main','client')
+utils.generate_keys()
 utils.package_folder('client/','packages/data_collector')
 server.listen()
