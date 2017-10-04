562700aa3e257cd7f770ca8a48690ad9470f478d
Bruno Bonando <bruno@bonandos.com>
2017-10-04 01:35:02 -0300

    Cryptodome implementation

--- config/utils.py
+++ config/utils.py
@@ -1,26 +1,20 @@
-from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Cipher import PKCS1_OAEP
 import random, string, os, zipfile
 
-secret_code=''
-private_key='./'
-public_key='./'
 
-def generate_keys(server_path,client_path):
+private_key = './main/private_rsa_key.pem'
+public_key  = './client/public_rsa_key.pem'
+
+def generate_keys():
     print('Bonandos:/# Generating keys                ',end='')
     key = RSA.generate(2048)
-    secret_code = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    encrypted_key = key.exportKey(passphrase=secret_code, pkcs=8)    
-    private_key = '{}/private_rsa_key.pem'.format(server_path)
-    public_key  = '{}/public_rsa_key.pem'.format(client_path)
     with open(private_key, 'wb') as f:
-        f.write(encrypted_key)        
+        f.write(key.exportKey())        
     with open(public_key,'wb') as f:
         f.write(key.publickey().exportKey())
     print('[OK]')
-    # print(encrypt('bonando'))
-    
 
 def _zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
@@ -35,20 +29,14 @@ def package_folder(path,name):
     print('[OK]')
 
 def encrypt(str):
-    recipient_key = RSA.import_key(open(public_key).read())
-    session_key = get_random_bytes(16)
-    cipher_rsa = PKCS1_OAEP.new(recipient_key)
-    cipher_aes = AES.new(session_key, AES.MODE_EAX)
-    ciphertext, tag = cipher_aes.encrypt_and_digest(str)
-    return '{}{}{}{}'.format(cipher_rsa.encrypt(session_key),cipher_aes.nonce,tag,ciphertext)
+    key = RSA.importKey(open(public_key).read())
+    cipher = PKCS1_OAEP.new(key)
+    return cipher.encrypt(str.encode('utf-8'))
     
 def decrypt(str):
-    rsa_private_key = RSA.import_key(open(private_key).read(),passphrase=secret_code)
-    enc_session_key, nonce, tag, ciphertext = [fobj.read(x) for x in (rsa_private_key.size_in_bytes(), 16, 16, -1)]
-    cipher_rsa = PKCS1_OAEP.new(rsa_private_key)
-    session_key = cipher_rsa.decrypt(enc_session_key)
-    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-    return cipher_aes.decrypt_and_verify(ciphertext, tag)
+    key = RSA.importKey(open(private_key).read())
+    cipher = PKCS1_OAEP.new(key)
+    return cipher.decrypt(str).decode()
 
 if __name__ == '__main__':
     print('You should not run this file! Run App.py instead.')   
